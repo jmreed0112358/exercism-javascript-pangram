fcd44e31562eca0ef11c183e66b90f569a633272
Jeremy M. Reed <reeje76@gmail.com>
2016-08-13 15:32:44 -0600

    Implement sanitize().

--- pangram.js
+++ pangram.js
@@ -4,7 +4,7 @@ var xregexp = require('xregexp'),
   InvalidParameterException = require('./exceptions/InvalidParameterException.js');
 
 var Pangram = function(sentence) {
-  throw new NotImplementedException();
+  this.sentence = sentence;
 };
 
 Pangram.prototype.isPangram = function() {
@@ -15,6 +15,21 @@ Pangram.prototype.isPangram = function() {
  *  Trims, removes unprintable control characters 
  *  and reduces alphabetic chars to lower case.
  */
-Pangram.prototype.sanitize = function(sentence) {
-  throw new NotImplementedException();
+Pangram.prototype.sanitize = function(str) {
+  var i = 0,
+    result = '',
+    regex;
+  xregexp.install('astral');
+  regex = xregexp('^\\pC+$');
+
+  str = str.toLowerCase();
+  for (i = 0 ; i < str.length ; i++ ) {
+    if (!regex.test(str[i])) {
+      result = result + str[i];
+    }
+  }
+
+  return validator.trim(result).replace(/  +/g, ' ');
 };
+
+module.exports = Pangram;
