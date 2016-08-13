c0d46a9ec95fd4b9fb7521f091a5e1eca59a085c
Jeremy M. Reed <reeje76@gmail.com>
2016-08-13 16:22:17 -0600

    Activate tests.

--- pangram.spec.js
+++ pangram.spec.js
@@ -2,55 +2,55 @@ var Pangram = require('./pangram');
 
 const UNPRINTABLE_CHARS = '\u0000\u0001\u0002\u0003\u0004\u0005\u0006\u0007\b\t\n\u000b\f\r\u000e\u000f\u0010\u0011\u0012\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001a\u001b\u001c\u001d\u001e\u001f';
 
-xdescribe('Pangram()', function()  {
+describe('Pangram()', function()  {
 
   it('empty sentence', function() {
     var pangram = new Pangram('');
     expect(pangram.isPangram()).toBe(false);
   });
 
-  xit('pangram with only lower case', function()  {
+  it('pangram with only lower case', function()  {
     var pangram = new Pangram("the quick brown fox jumps over the lazy dog");
     expect(pangram.isPangram()).toBe(true);
   });
 
-  xit("missing character 'x'", function()  {
+  it("missing character 'x'", function()  {
     var pangram = new Pangram("a quick movement of the enemy will jeopardize five gunboats");
     expect(pangram.isPangram()).toBe(false);
   });
 
-  xit("another missing character 'x'", function() {
+  it("another missing character 'x'", function() {
     var pangram = new Pangram("the quick brown fish jumps over the lazy dog");
     expect(pangram.isPangram()).toBe(false);
   });
 
-  xit("pangram with underscores", function() {
+  it("pangram with underscores", function() {
     var pangram = new Pangram("the_quick_brown_fox_jumps_over_the_lazy_dog");
     expect(pangram.isPangram()).toBe(true);
   });
 
-  xit("pangram with numbers", function() {
+  it("pangram with numbers", function() {
     var pangram = new Pangram("the 1 quick brown fox jumps over the 2 lazy dogs");
     expect(pangram.isPangram()).toBe(true);
   });
 
-  xit('missing letters replaced by numbers', function() {
+  it('missing letters replaced by numbers', function() {
     var pangram = new Pangram("7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog");
     expect(pangram.isPangram()).toBe(false);
   });
 
-  xit('pangram with mixed case and punctuation', function()  {
+  it('pangram with mixed case and punctuation', function()  {
     var pangram = new Pangram("\"Five quacking Zephyrs jolt my wax bed.\"");
     expect(pangram.isPangram()).toBe(true);
   });
 
-  xit('pangram with non-ascii characters', function()  {
+  it('pangram with non-ascii characters', function()  {
     var pangram = new Pangram("Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich.");
     expect(pangram.isPangram()).toBe(true);
   });
 });
 
-describe('sanitize()', function() {
+xdescribe('sanitize()', function() {
 
   it('tokenizes, returns the expected result', function() {
     var pangram = new Pangram('');
